936565438209198461e962a38dff45a6bff035c9
limdp99 <dlim@pm-studios.com>
2022-06-16 14:48:52 +0900

    init

---
+++ precache-manifest.75042702d00c461aff0f86243ac2426e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13377aea2dcc19de761c82389ceb24bd",
+    "url": "/redeem_hoa/index.html"
+  },
+  {
+    "revision": "8e6a0ec314061b5b7cff",
+    "url": "/redeem_hoa/static/css/main.afcc04b8.chunk.css"
+  },
+  {
+    "revision": "a74199fbe243160d88dd",
+    "url": "/redeem_hoa/static/js/2.6c6a24be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/redeem_hoa/static/js/2.6c6a24be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e6a0ec314061b5b7cff",
+    "url": "/redeem_hoa/static/js/main.314ef4ad.chunk.js"
+  },
+  {
+    "revision": "9eb405949ce566ba057f",
+    "url": "/redeem_hoa/static/js/runtime-main.4117af50.js"
+  }
+]);
